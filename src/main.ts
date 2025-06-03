30e349f6dce4c9a7e45fd464ffb0473fcc74275d
Blaaaxo <blaaaa.xo0@gmail.com>
2025-06-03 14:28:53 -0500

    agregado soporte para CORS en la configuración del servidor

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 4000);
+
+  // Configurar CORS
+  app.enableCors(
+    {
+      origin: process.env.CORS_ORIGIN ?? 'http://localhost:3000',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    }
+  );
 }
 bootstrap();
