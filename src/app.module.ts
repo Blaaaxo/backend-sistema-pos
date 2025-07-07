987faf09611a062ca3bf59647a38e9f511586487
Blaaaxo <blaaaa.xo0@gmail.com>
2025-07-07 07:10:40 -0500

    agregado modules que faltaban en app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,11 @@ import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
 import { OrdenComprasModule } from './compras/orden-compras/orden-compras.module';
+import { IngresoComprasModule } from './compras/ingreso-compras/ingreso-compras.module';
+import { DevolucionComprasModule } from './compras/devolucion-compras/devolucion-compras.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, PrismaModule, OrdenComprasModule],
+  imports: [AuthModule, UsersModule, PrismaModule, OrdenComprasModule, IngresoComprasModule, DevolucionComprasModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
